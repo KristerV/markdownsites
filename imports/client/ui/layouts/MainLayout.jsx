9b1e35ca1120ffae6fe4dbf0e0ee64064140b134
Krister Viirsaar <krister.viirsaar@gmail.com>
2016-07-02 13:44:35 +0300

    random color maker

--- imports/client/ui/layouts/MainLayout.jsx
+++ imports/client/ui/layouts/MainLayout.jsx
@@ -1,10 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
+import '../styles/dev.less';
 
-export default class extends Component {
+export default class extends React.Component {
 
 	render() {
-		return (
-			<div>MainLayout</div>
+		return (<div className="wh100">
+				<nav className="dev-bg"></nav>
+				<main className="dev-bg"></main>
+			</div>
 		)
 	}
 
