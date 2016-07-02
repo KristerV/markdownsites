39d0c23d96f56700350feab8157e333f5586968e
Krister Viirsaar <krister.viirsaar@gmail.com>
2016-07-02 14:23:56 +0300

    main layout

---
+++ imports/client/ui/components/NavBar.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+export default class extends React.Component {
+
+	render() {
+		return (
+			<div>
+				<h2>Main</h2>
+				<p>Preview</p>
+				<p>Save</p>
+				<p>Publish</p>
+				<p>About</p>
+
+				<h2>Writing</h2>
+				<p>form elements</p>
+				<p>Layout</p>
+				<p>Markdown cheatsheet</p>
+
+				<h2>Settings</h2>
+				<p>domain</p>
+				<p>link to my code</p>
+				<p>change owner</p>
+				<p>update website code</p>
+
+				<h2>My sites</h2>
+				<p>site 1</p>
+				<p>site 2</p>
+				<p>site 3</p>
+				<p>New site</p>
+			</div>
+		)
+	}
+
+}
--- imports/client/ui/layouts/MainLayout.jsx
+++ imports/client/ui/layouts/MainLayout.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
+
 import '../styles/dev.less';
+import '../styles/layout.less';
+import './MainLayout.less';
+
+import NavBar from '../components/NavBar.jsx';
 
 export default class extends React.Component {
 
 	render() {
-		return (<div className="wh100">
-				<nav className="dev-bg"></nav>
-				<main className="dev-bg"></main>
+		return (<div className="wh100 MainLayout">
+				<nav className="dev-bg h100 float-left">
+					<NavBar/>
+				</nav>
+				<main className="dev-bg h100 float-left">
+					{this.props.content}
+				</main>
 			</div>
 		)
 	}
