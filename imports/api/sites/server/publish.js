75d0b44789b1041c592a035c21ca5b8b6143a4cc
Krister Viirsaar <krister.viirsaar@gmail.com>
2016-07-03 15:32:23 +0300

    domain find

--- imports/api/sites/server/publish.js
+++ imports/api/sites/server/publish.js
@@ -1,6 +1,6 @@
 Meteor.publish('sites.single', function(siteId) {
 	check(siteId, String);
-	return SitesCollection.find(siteId);
+	return SitesCollection.find({$or: [{_id: siteId}, {domain: siteId}]});
 });
 
 Meteor.publish('sites.list', function() {
