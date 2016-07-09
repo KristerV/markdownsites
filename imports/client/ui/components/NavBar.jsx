f4346d36d5a557f9686a21dc2902929479328ebb
Krister Viirsaar <krister.viirsaar@gmail.com>
2016-07-09 15:32:26 +0300

    cleanup links

--- imports/client/ui/components/NavBar.jsx
+++ imports/client/ui/components/NavBar.jsx
@@ -10,20 +10,16 @@ export default class extends React.Component {
 		const domain = siteId && siteId.includes('.') ? siteId : null; // Data isn't here so use router params
 		return (
 			<div>
-				{domain ? // If domain set
-					<ol className="no-bullets">
-						<li>{domain} [switch]</li>
-					</ol>
-				: null}
 
 				<ol className="no-bullets">
-					<li><a href={linkWriter}>Writer</a></li>
-					<li><a href={linkPreview}>Preview</a></li>
-					<li><a href={linkAbout}>About</a></li>
+					<li><a href={linkAbout}>Site1</a></li>
+					<li><a href={linkAbout}>Site2</a></li>
+					<li><a href={linkAbout}>New site</a></li>
 				</ol>
 
 				<ol className="no-bullets">
-					<li>Markdown cheatsheet</li>
+					<li><a href={linkAbout}>About</a></li>
+					<li>Markdown on/off</li>
 				</ol>
 
 			</div>
