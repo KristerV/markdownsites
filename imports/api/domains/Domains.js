696868e83969df0e211ea7801f311b80ac769c91
Krister Viirsaar <krister.viirsaar@gmail.com>
2016-08-10 16:43:19 +0300

    reduce domain upates

--- imports/api/domains/Domains.js
+++ imports/api/domains/Domains.js
@@ -5,6 +5,6 @@ Domains = {
 };
 
 Meteor.startup(() => {
-	Meteor.setTimeout(Domains.updatePrices, 1000 * 60 * 10);
+	Meteor.setTimeout(Domains.updatePrices, 1000 * 60 * 1);
 	Meteor.setInterval(Domains.updatePrices, 1000 * 60 * 60 * 24 * 3); // every 3 days
 })
