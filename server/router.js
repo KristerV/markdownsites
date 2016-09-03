e337f5ca652a0f13cd2e9cc78c653859709c3733
Krister Viirsaar <krister.viirsaar@gmail.com>
2016-09-03 14:38:01 +0300

    body-parser

--- server/router.js
+++ server/router.js
@@ -1,6 +1,11 @@
+import bodyParser from 'body-parser';
+
+Picker.middleware( bodyParser.json() );
+Picker.middleware( bodyParser.urlencoded( { extended: false } ) );
 
 Picker.route( '/braintree-webhooks', function( params, request, response, next ) {
-	console.log(" ---------------- Braintre webhook ---------------- ");
+	console.log(" ---------------- Braintree webhook ---------------- ");
 	console.log(params);
-	console.log(request);
+	console.log(request.body);
+	response.end('nothing');
 });
