edb16cf19fb2ec5c88c90258a5286f8981336324
Krister Viirsaar <krister.viirsaar@gmail.com>
2016-09-04 12:08:37 +0300

    address

--- imports/client/ui/components/About.jsx
+++ imports/client/ui/components/About.jsx
@@ -5,7 +5,10 @@ export default class extends React.Component {
 	render() {
 		return (
 			<div>
-				This site is still under construction. You can contact krister.viirsaar@gmail.com with questions.
+				<p>
+					This site is still under construction.<br/>You can contact krister.viirsaar@gmail.com with questions.
+				</p>
+				<p>Täp OÜ, 12125250<br/>Vilmsi 29-4, Tallinn 10126, Estonia</p>
 			</div>
 		)
 	}
