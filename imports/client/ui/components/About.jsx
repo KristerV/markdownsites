a2e592faf496795bff6cd7ca725953e848904519
Krister Viirsaar <krister.viirsaar@gmail.com>
2016-09-07 10:58:38 +0300

    terms

--- imports/client/ui/components/About.jsx
+++ imports/client/ui/components/About.jsx
@@ -8,7 +8,15 @@ export default class extends React.Component {
 				<p>
 					This site is still under construction.<br/>You can contact krister.viirsaar@gmail.com with questions.
 				</p>
+				<h3>Company details</h3>
 				<p>Täp OÜ, 12125250<br/>Vilmsi 29-4, Tallinn 10126, Estonia</p>
+				<h3>Terms and Conditions</h3>
+				<ol>
+					<li>Any domain bought through this site will ultimately belong to the end client. Upon request and payment of transfer fees the domain will be transfered to the end-user.</li>
+					<li>Any illegal or  content will be removed upon notification of such content.</li>
+					<li>Prices may change at any time, but the subscriptions will remain as they are, unless the domain service-providers change their prices. In the latter case anyone affected will be notified 30-days in advance.</li>
+					<li>Any content the user publishes through this site will be publicliy available for all to see. Well, such is the point of this website in the first place.</li>
+				</ol>
 			</div>
 		)
 	}
