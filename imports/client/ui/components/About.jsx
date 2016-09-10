31cf83ac71d6d76d030fae83959e5474eb119c55
Krister Viirsaar <krister.viirsaar@gmail.com>
2016-09-10 11:57:19 +0300

    pricing explicit

--- imports/client/ui/components/About.jsx
+++ imports/client/ui/components/About.jsx
@@ -17,6 +17,17 @@ export default class extends React.Component {
 					<li>Prices may change at any time, but the subscriptions will remain as they are, unless the domain service-providers change their prices. In the latter case anyone affected will be notified 30-days in advance.</li>
 					<li>Any content the user publishes through this site will be publicliy available for all to see. Well, such is the point of this website in the first place.</li>
 				</ol>
+				<h3>Pricing</h3>
+				<p>Publishing a site and connecting it to a pre-owned domain is free. Buying a domain and setting the DNS up is not. Here's a list of the most popular domains and their prices.</p>
+				<ul>
+					<li>.com - $16 a year</li>
+					<li>.co.uk - $13 a year</li>
+					<li>.org - $17 a year</li>
+					<li>.net - $17 a year</li>
+					<li>.info - $15 a year</li>
+					<li>.io - $64 a year</li>
+				</ul>
+				<p>Subscriptions are not automatic, you pay for one year at a time. So if you don't want to continue owning the domain no action is needed. Your credit card info is not saved anywhere.</p>
 			</div>
 		)
 	}
