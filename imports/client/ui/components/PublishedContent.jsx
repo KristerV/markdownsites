69a24418f5be28572025fe7600fb3b43080d0aa2
Krister Viirsaar <krister.viirsaar@gmail.com>
2016-11-11 10:19:56 +0200

    scroller menu

--- imports/client/ui/components/PublishedContent.jsx
+++ imports/client/ui/components/PublishedContent.jsx
@@ -28,8 +28,87 @@ export default class extends React.Component {
 				}.bind(this)
 			});
 		this.toggleMenu = this.toggleMenu.bind(this);
+		this.componentDidMount = this.componentDidMount.bind(this);
+		this.refreshTOC = this.refreshTOC.bind(this);
 		this.state = {
 			menuRevealed: false
+		}
+	}
+
+	componentDidMount() {
+
+		// Scroll into position after  content has loaded
+		if( window.location.hash != '' ){
+			var anchor = $( location.hash ).get(0);
+			if(anchor){
+				anchor.scrollIntoView();
+				document.body.scrollTop -= 25;
+			}
+		}
+
+		// Find title visible on screen
+		this.timer = Meteor.setInterval(() => {
+
+			var winTop = $('body').scrollTop();
+			var winHeight = $(window).height();
+			var headers = $(':header:not(.table-of-contents)');
+
+			// Find first visible
+			var firstVisible;
+			for (var i = 0; i < headers.length; i++) {
+				var elem = $(headers[i]);
+				var top = elem.position().top;
+				if (top < winTop)
+					firstVisible = elem;
+				else
+					break;
+			}
+
+			// Change hash
+			var topic = '';
+			if (firstVisible) {
+				topic = firstVisible.attr("id");
+			}
+			history.pushState(null, null, '#'+topic);
+			this.refreshTOC();
+		},1000);
+
+		// Change menu when menu is clicked
+		$(window).on('hashchange',() => {
+			this.refreshTOC();
+		});
+	}
+
+	componentWillUnmount() {
+		Meteor.clearInterval(this.timer);
+		$(window).off('hashchange');
+	}
+
+	refreshTOC() {
+		// Collapse menu
+		$('.table-of-contents .active').removeClass('active');
+
+		// Get hash from URL
+		var placeholder = location.hash.slice(1);
+		var link = $('.table-of-contents a[href="#'+placeholder+'"]');
+
+		// Mark link and reveal parent ul's
+		link.addClass('active').parents('ul:not(.table-of-contents > ul)').addClass('active');
+
+		// Get link clicked and reveal menu under it (it's actually next to it)
+		var nextUl = link.parent('li').addClass('active');
+
+		// If no direct li's, reveal next ul's
+		var children = nextUl.children();
+		var isLi = false
+		for (var i = 0; i < children.length; i++) {
+			if (children[i].tagName === 'LI') {
+				isLi = true;
+				break;
+			}
+		}
+		if (!isLi) {
+			nextUl.children('ul').addClass('active');
 		}
 	}
 
@@ -43,7 +122,7 @@ export default class extends React.Component {
 		const html = this.markit.render("[[toc]]\n" + markdown);
 		return (<div className={"PublishedContent" + (this.state.menuRevealed ? ' revealed' : '')}>
 			<button className={"revealer button ui" + (this.state.menuRevealed ? '' : ' basic')} onClick={this.toggleMenu}>MENU</button>
-			<div onClick={this.toggleMenu} dangerouslySetInnerHTML={{__html: html}}></div>
+			<div dangerouslySetInnerHTML={{__html: html}}></div>
 			<div className="shade" onClick={this.toggleMenu}></div>
 		</div>);
 	}
