17429c6cbb672e76ead4f2264b9bfdf98c3fad5a
Krister Viirsaar <krister.viirsaar@gmail.com>
2016-12-10 23:15:50 +0200

    comments

--- imports/client/ui/components/MediumEditor.jsx
+++ imports/client/ui/components/MediumEditor.jsx
@@ -11,32 +11,42 @@ export default class extends React.Component {
 
 	constructor(props) {
 		super(props);
+
+		// Markdown->HTML converter
 		this.markit = new MarkdownIt({
 			linkify: true,
 			breaks: true,
 		});
+
+		// HTML->Markdown converter
 		this.und = new upndown();
 	}
 
 	render() {
+
+		// Basically a placeholder with default text
 		const text = this.markit.render(this.props.markdown);
 		return(<div dangerouslySetInnerHTML={{__html: text}}></div>)
 	}
 
 	componentDidMount() {
-		const dom = ReactDOM.findDOMNode(this);
 
+		// Editor options
 		let options = this.props.options || {};
 		options.imageDragging = false;
 		options.targetBlank = true;
 		options.toolbar = {
 			buttons: ['bold', 'italic', 'h1', 'h2', 'h3', 'anchor', 'image', 'orderedlist', 'unorderedlist', 'quote']
 		};
-
 		options.extensions = {
 			imageDragging: {},
 		};
+
+		// Create the editor
+		const dom = ReactDOM.findDOMNode(this);
 		this.medium = new MediumEditor(dom, options);
+
+		// Respond to updates
 		this.medium.subscribe('editableInput', (event, editable) => {
 		    this.und.convert(editable.innerHTML, (err, res) => {
 		    	if (err)
